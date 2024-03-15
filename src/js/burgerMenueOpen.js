0c048cc02ec3c23aa040b92e83168975ade1fdae
SirotovAlexander <alexander.sirotov@gmail.com>
2024-03-15 12:12:22 +0200

    update
    update

---
+++ src/js/burgerMenueOpen.js
@@ -0,0 +1,9 @@
+const buttonOpen = document.querySelector('.burger-Button');
+const burgerMenue = document.querySelector('#menu-open');
+
+// buttonOpen.addEventListener('click', openModal);
+
+// function openModal(e) {
+//   console.dir(burgerMenue.checked);
+
+// }
